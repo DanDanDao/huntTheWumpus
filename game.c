3ebb63c0d25486fcf5c2f6ba00da1d7f73265a1c
Daniel Dao <guangdanny@gmail.com>
2018-08-22 11:32:03 +1000

    super dooper

--- game.c
+++ game.c
@@ -5,7 +5,9 @@
 */
 #include "game.h"
 
-void game_PlayGame(){
+void menuAfterMain() {
+
+	char enter = "/0";
 
 	printf(
 		"You can use the following commands to play the game:\n"
@@ -18,25 +20,39 @@ void game_PlayGame(){
 		"south (or s)\n"
 		"east (or e)\n"
 		"west (or w)\n"
-		"quit\n"
+		"quit\n\n"
 	);
 
-	char userInput[INPUT_MAX_LENGTH + EXTRA_SPACES];
+	printf("Press enter to continue...");
 
-	getInput("Press return to continue...\n", userInput, sizeof(userInput));
-
-	if(strstr(userInput, "\n")!=NULL) {
-		getInput(
-			"At this stage of the program only two commands are acceptable:\n"
-			"load <g>\n"
-			"quit\n", userInput, sizeof(userInput));
-			if(strcmp(userInput, "load 1")!=0) {
-				board_Load(BOARD_1,BOARD_1);
-			} else if (strcmp(userInput, "load 2")!=0) {
-				board_Load(BOARD_2,BOARD_2);
-			} else if (strcmp(userInput,"quit")!=0) {
-				return;
-			}
-			srand(0);
-		}
+	/* If enter is not equal to the carriage return character and the new userInput character,
+	run the while loop to getChar() the enter variable. */
+	while(enter != '\r' && enter != '\n')
+	{
+		enter = getchar();
 	}
+	printf("\n");
+}
+
+void game_PlayGame(){
+
+	// char userInput[100];
+
+	menuAfterMain();
+
+	// if(strstr(userInput, "\n")!=NULL) {
+	// 	getInput(
+	// 		"At this stage of the program only two commands are acceptable:\n"
+	// 		"load <g>\n"
+	// 		"quit\n", userInput, sizeof(userInput));
+	//
+	// 		if(strcmp(userInput, "load 1")!=0) {
+	// 			board_Load(BOARD_1,BOARD_1);
+	// 		} else if (strcmp(userInput, "load 2")!=0) {
+	// 			board_Load(BOARD_2,BOARD_2);
+	// 		} else if (strcmp(userInput,"quit")!=0) {
+	// 			return;
+	// 		}
+	// 		srand(0);
+	// 	}
+}
